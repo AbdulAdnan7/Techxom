ac758a0e16f923b4c180134134ff5941bcb4b955
Abdul Adnan <peerabduladnan@gmail.com>
2025-07-09 14:38:16 +0530

    Completed the project

--- Frontend/src/Components/Products.jsx
+++ Frontend/src/Components/Products.jsx
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import toast, {Toaster} from 'react-hot-toast';
+import { useCart } from '../context/CartContext';
 
 const Products = () => {
     const [products, setProducts] = useState([]);
-    const [selectedCateogary, setSelectedCateogary] = useState('All')
+    const [selectedCateogary, setSelectedCateogary] = useState('All');
+
+   const { addToCart, removeFromCart, cartItems } = useCart()
 
     const category = ['All', 'Audio', 'Laptops', 'Phones', 'Accessories']
 
@@ -14,7 +18,19 @@ const Products = () => {
                 setProducts(data);
             })
             .catch(err => console.log('Error', err))
-    }, [])
+    }, []);
+
+    const isInCart = (id) => cartItems.some(item => item.id === id);
+
+    const handleAdd = (product) => {
+        addToCart(product);
+        toast.success(`${product.name} added to cart`)
+    }
+
+    const handleRemove = (id) => {
+        removeFromCart(id);
+        toast.error('Item removed from cart')
+    }
 
     const filterProduct = selectedCateogary === 'All' ? products : products.filter((p) => 
         p.category?.toLowerCase().trim() === selectedCateogary.toLowerCase()
@@ -22,6 +38,7 @@ const Products = () => {
 
     return (
         <section className='mb-20'>
+            <Toaster position='top-right' />
             <div className='flex justify-center mt-10'>
                 <h1 className='text-2xl'>All Products</h1>
             </div>
@@ -52,9 +69,17 @@ const Products = () => {
                                 <p className='text-sm text-gray-600 truncate mb-2'>{product.description}</p>
                                 <p className='text-rose-600 font-bold mb-2'>${product.price}</p>
                                 <div>
-                                    <button className="bg-rose-600 text-white px-3 py-1 text-sm rounded hover:bg-rose-700 transition">
+                                    { isInCart(product.id) ? (
+                                        <button
+                                        className='bg-gray-800 text-white px-3 py-1 text-sm rounded hover:bg-gray-800 transition'
+                                        onClick={() => handleRemove(product.id)} >Remove from Cart</button>
+                                    ) :
+                                    (
+                                    <button onClick={() => handleAdd(product)} className="bg-rose-600 text-white px-3 py-1 text-sm rounded hover:bg-rose-700 transition">
                                         Add to Cart
                                     </button>
+                                    )
+                }
                                 </div>
                             </div>
                         </div>
